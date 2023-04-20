d4e30e11ce4f5f08e0eeb13fa9b0bfce46cb3cdd
Sapasetty <sirisha.sapasetty@dxc.com>
2023-04-20 12:34:43 +0530

    New One

---
+++ Csharp/Largestof3/Largestof3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Largestof3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            int num3 = Convert.ToInt16(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine("num1 is greater");
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("num2 is greater");
+            }
+            else
+            {
+                Console.WriteLine("num3 is greater");
+            }
+
+        }
+    }
+}
---
+++ Csharp/Vowels/Vowels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+namespace Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the char");
+            char vowel = Convert.ToChar(Console.ReadLine());
+            if (vowel == 'A' || vowel == 'E' || vowel == 'I' || vowel == 'O' || vowel == 'U' || vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u') 
+            {
+                Console.WriteLine("Character is Vowel");
+            }
+            else
+            {
+                Console.WriteLine("It's a Consonant");
+            }
+        }
+    }
+}
